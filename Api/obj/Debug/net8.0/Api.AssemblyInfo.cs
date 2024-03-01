f2f1a1b811325ca3e5bf3047fad74ba06da70948
Jean <Jean-OIKONOMOU>
2024-03-01 23:15:44 +0100

    added POST method and small local sqlite db

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa174f9e92a359ffa8b152c22ba0a238c02b2ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08065318fcc30c826cbce0f5bcd088eef34eb87d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
